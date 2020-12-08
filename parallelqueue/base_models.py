afe12f4788fe776d86cb480cda3a9104506372b0
Aaron <ajstone@uwaterloo.ca>
2020-12-08 00:48:20 -0500

    Added general distributions

--- parallelqueue/base_models.py
+++ parallelqueue/base_models.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd
 from simpy import Environment, Resource
 
-from parallelqueue.components import PoissonArrivals
+from parallelqueue.components import PoissonArrivals, GeneralArrivals
 
 
 class RedundancyQueueSystem:
@@ -12,16 +12,17 @@ class RedundancyQueueSystem:
     service. After completing service, each job and its replicas are disposed of.
 
     :param numberJobs: Max number of jobs if infiniteJobs is False. Will be ignored if infiniteJobs is True.
-    :param arrivalMean: Mean of Poisson arrival process.
-    :param maxTime: Runtime limit.
     :param parallelism: Number of queues in parallel.
     :param seed: Random number generation seed.
     :param r: Threshold. Should be set to an integer, defaulting to :code:`None` otherwise.
     :param infiniteJobs: If true, there will be no upper limit for the number of jobs generated.
     :param df: Whether or not a pandas.DataFrame of the queue sizes over time should be returned.
     :param d: Number of queues to parse.
-    :param meanServer: Mean servicing time.
     :param doPrint: If true, each event will trigger a statement to be printed.
+    :param Arrival: A kwarg specifying the arrival distribution to use (a function).
+    :param AArgs: parameters needed by the function.
+    :param Service: A kwarg specifying the service distribution to use (a function).
+    :param SArgs: parameters needed by the function.
 
     Example
     -------
@@ -29,34 +30,36 @@ class RedundancyQueueSystem:
 
         # Specifies a SimPy environment consisting
         # of a Redundancy-2 queueing system and a Poisson arrival process.
-        sim = ParallelQueueSystem(arrivalMean=5, maxTime=100.0,
-            parallelism=100, seed=1234, d=2, meanServer=1.0)
+        sim = RedundancyQueueSystem(maxTime=100.0,
+            parallelism=100, seed=1234, d=2,
+            Arrival=random.expovariate, AArgs=0.5,
+            Service=random.expovariate, SArgs=1)
         sim.RunSim
 
     """
 
-    def __init__(self, arrivalMean, maxTime, parallelism, seed, d, meanServer, r=None, doPrint=False, df=False,
-                 infiniteJobs=True, numberJobs=0):
+    def __init__(self, maxTime, parallelism, seed, d, r=None, doPrint=False, df=False,
+                 infiniteJobs=True, numberJobs=0, **kwargs):
         self.infiniteJobs = infiniteJobs
         self.d = d
         self.doPrint = doPrint
-        self.timeInBank = meanServer
         self.seed = seed
         self.r = r
         self.parallelism = parallelism
         self.maxTime = maxTime
-        self.arrivalMean = arrivalMean
         self.ReplicaDict = {}
         self.Number = 0 if self.infiniteJobs else numberJobs
         self.queuesOverTime = []
         self.df = df
+        self.kwargs = kwargs
 
     def __SimManager(self):
         random.seed(self.seed)
         env = Environment()
         queues = {i: Resource(env) for i in range(self.parallelism)}
 
-        env.process(PoissonArrivals(system=self, env=env, number=self.Number, interval=self.arrivalMean, queues=queues))
+        env.process(GeneralArrivals(system=self, env=env, number=self.Number,
+                                    queues=queues, **self.kwargs))
         print(f'\n Running simulation with seed {self.seed}... \n')
         env.run(until=self.maxTime)
         print('\n Done \n')
@@ -98,7 +101,7 @@ class JSQSystem:
 
         # Specifies a SimPy environment consisting
         # of a JSQ-2 queueing system and a Poisson arrival process.
-        sim = ParallelQueueSystem(arrivalMean=5, maxTime=100.0,
+        sim = RedundancyQueueSystem(arrivalMean=5, maxTime=100.0,
             parallelism=100, seed=1234, r=10, meanServer=1.0)
         sim.RunSim
 
