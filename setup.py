2ebd1d74dd5f39fbe779da8aa990985cde8cf89e
Aaron <ajstone@uwaterloo.ca>
2020-12-11 15:10:15 -0500

    Added tests to show able to retrieve results from pure simpy

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ParallelQueue',
-    version='0.0.6',
+    version='0.0.6.2',
     packages=['parallelqueue'],
     url='https://github.com/aarjaneiro/ParallelQueue',
     project_urls='https://parallelqueue.readthedocs.io/en/latest/',
