8cc2e0b3dc8a9d5a83835363063d1d579eeb055e
Aaron <ajstone@uwaterloo.ca>
2020-12-13 18:47:44 -0500

    removed __init__ contents

--- parallelqueue/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-The module :code:`parallelqueue` provides the basic processes used in parallel queue models.
-"""
-from components import Arrivals, Job, JobRouter
-
-__all__ = [Arrivals, Job, JobRouter]
